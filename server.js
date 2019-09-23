8469e344cc15efcf4c8d756f0f1bece93caf2552
Victor-Dev77 <deyanovitch77580@gmail.com>
2019-09-23 12:34:52 +0200

    add POST request /chat

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/chat', (req, res) => {
+	if (req.body.msg == "ville") {
+		console.log("Nous sommes à Paris")
+		res.send("Nous sommes à Paris")
+	}
+	else if (req.body.msg == "meteo") {
+		console.log("Il fait beau")
+		res.send("Il fait beau")
+	}
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
