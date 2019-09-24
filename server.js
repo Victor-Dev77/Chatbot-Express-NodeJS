35a8c536d082e02969ec908169c7302e81942997
Victor-Dev77 <deyanovitch77580@gmail.com>
2019-09-24 15:26:29 +0200

    Connect MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,74 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json());
+
+(async function() {
+  // Connection URL
+  const url = 'mongodb://localhost:27017/chat-bot';
+  // Database Name
+  const dbName = 'chat-bot';
+  const client = new MongoClient(url, { useNewUrlParser: true });
+
+  try {
+    // Use connect method to connect to the Server
+   // await client.connect();
+   // console.log("Connected correctly to server");
+
+    //const db = client.db(dbName);
+
+    // Get the collection
+   // const col = db.collection('messages');
+    
+    /*let r = await col.insertOne({ date: new Date() });
+    assert.equal(1, r.insertedCount);
+
+    const docs = await col.find({}).toArray();
+    console.log(docs);
+	*/
+	app.get('/messages/all', async function (req, res) { 
+		try {
+			await client.connect();
+			const db = client.db(dbName);
+			const col = db.collection('messages');
+			let docs = await col.find({}).toArray();
+			console.log(docs);
+			if (docs.length == 0) {
+				let items = 
+				[
+  					{
+    					from: 'user',
+    					msg: 'demain',
+  					},
+  					{
+    					from: 'bot',
+    					msg: 'Demain: Mercredi',
+  					}
+				];
+				let r = await col.insertMany(items);
+				assert.equal(2, r.insertedCount);
+				res.send(items);
+			}
+			else {
+				res.send(docs);
+			}
+			client.close();
+		} catch (err) {
+			res.send(err.message);
+		}
+		
+	});
+
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+ // client.close();
+})();
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -18,7 +84,7 @@ app.post('/chat', (req, res) => {
 })
 
 app.get('/hello', (req, res) => {
-	let name = req.param('nom');
+	let name = req.query.country;
 	console.log(name)
 	if (name != undefined && name != "") {
 		res.send("Bonjour, " + name)
